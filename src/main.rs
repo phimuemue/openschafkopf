ce3758588773a138520f4643c307c2160b51918b
philipp <philipp>
2015-10-31 08:47:39 +0100

    Add main routine

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+mod card;
+mod stich;
+mod combinatorics;
+mod cardvectorparser;
+mod hand;
+mod rules;
+mod rulesrufspiel;
+mod gamestate;
+mod game;
+mod player;
+mod playercomputer;
+
+use card::CCard;
+use card::EFarbe::*;
+use card::ESchlag::*;
+use hand::*;
+use cardvectorparser::*;
+use gamestate::*;
+use game::*;
+
+fn main() {
+    let mut stich = stich::CStich::new(2);
+    stich.zugeben(CCard::new(efarbeEICHEL, eschlagU));
+    stich.zugeben(CCard::new(efarbeGRAS, eschlag7));
+    for (i_player, card) in stich.indices_and_cards() {
+        println!("{} hat {}", i_player, card);
+    }
+    println!("{}", stich);
+    let hand = CHand::new_from_vec(
+        vec!(
+            CCard::new(efarbeEICHEL, eschlagU),
+            CCard::new(efarbeHERZ, eschlagK),
+            CCard::new(efarbeSCHELLN, eschlag7),
+        )
+    );
+    println!("{}", hand);
+    let hand2 = hand.new_from_hand(CCard::new(efarbeHERZ, eschlagK));
+    println!("{}", hand2);
+    let veccard = cardvectorparser::ParseCards("ek gk hz hu s7");
+    println!("Parsed cards");
+    for card in veccard {
+        println!("{}", card);
+    }
+}
