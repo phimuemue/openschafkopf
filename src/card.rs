f64ecaf28506ccf787bff929a3248e3a8df0ed20
philipp <philipp>
2015-12-31 16:05:26 +0100

    Nicer output for EFarbe

--- src/card.rs
+++ src/card.rs
@@ -33,7 +33,12 @@ impl quickcheck::Arbitrary for EFarbe {
 
 impl fmt::Display for EFarbe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{}", match self {
+            &efarbeEICHEL => "Eichel",
+            &efarbeGRAS => "Gras",
+            &efarbeHERZ => "Herz",
+            &efarbeSCHELLN => "Schelln",
+        } )
     }
 }
 
