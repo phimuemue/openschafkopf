21dc8fbd2c7bc2d2ea0ef0fb40dafe153742aaac
philipp <philipp>
2016-02-07 14:33:33 +0100

    Inlined some singly-used variables

--- src/playerhuman.rs
+++ src/playerhuman.rs
@@ -15,9 +15,8 @@ pub struct CPlayerHuman;
 
 impl CPlayer for CPlayerHuman {
     fn take_control(&mut self, gamestate: &SGameState, txcard: mpsc::Sender<CCard>) {
-        let eplayerindex = gamestate.which_player_can_do_something().unwrap();
         skui::print_vecstich(&gamestate.m_vecstich);
-        let ref hand = gamestate.m_ahand[eplayerindex];
+        let ref hand = gamestate.m_ahand[gamestate.which_player_can_do_something().unwrap()];
         txcard.send(
             skui::ask_for_alternative(
                 &format!("Your cards: {}", hand),
@@ -35,16 +34,15 @@ impl CPlayer for CPlayerHuman {
     }
 
     fn ask_for_game(&self, eplayerindex: EPlayerIndex, hand: &CHand) -> Option<Rc<TRules>> {
-        let vecorules = Some(None).into_iter() // TODO is there no singleton iterator?
+        skui::ask_for_alternative(
+            &format!("Your cards: {}. What do you want to play?", hand),
+            &Some(None).into_iter() // TODO is there no singleton iterator?
             .chain(
                 ruleset_default(eplayerindex).allowed_rules().iter()
                     .filter(|rules| rules.can_be_played(hand))
                     .map(|rules| Some(rules.clone()))
             )
-            .collect();
-        skui::ask_for_alternative(
-            &format!("Your cards: {}. What do you want to play?", hand),
-            &vecorules,
+            .collect::<Vec<_>>(),
             |orules| match orules {
                 &None => "Nothing".to_string(),
                 &Some(ref rules) => rules.to_string()
