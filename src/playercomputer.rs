88f055de350fc2fcb93778b27745a033b77ddcbb
philipp <philipp>
2016-02-14 23:56:28 +0100

    Improved and removed some comments

--- src/playercomputer.rs
+++ src/playercomputer.rs
@@ -12,9 +12,8 @@ pub struct CPlayerComputer;
 
 impl CPlayer for CPlayerComputer {
     fn take_control(&mut self, gamestate: &SGameState, txcard: mpsc::Sender<CCard>) {
-        //println!("CPlayerComputer {} taking control", gamestate.m_vecstich.last().unwrap().current_player_index());
         // TODO: implement some kind of strategy
-        // computer player immediately plays card by calling fn_play_card
+        // computer player immediately plays card
         txcard.send(
             gamestate.m_rules.all_allowed_cards(
                 &gamestate.m_vecstich,
@@ -24,7 +23,6 @@ impl CPlayer for CPlayerComputer {
                 ]
             )[0]
         ).ok();
-        //println!("CPlayerComputer {} played card", gamestate.m_vecstich.last().unwrap().current_player_index());
     }
 
     fn ask_for_game(&self, _eplayerindex: EPlayerIndex, _: &CHand) -> Option<Rc<TRules>> {
