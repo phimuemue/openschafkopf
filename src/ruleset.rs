9ec35f90adcb6bd1f5eca909d055deb91315225c
philipp <philipp>
2016-04-04 23:46:05 +0200

    Create .schafkopfruleset if not existing

--- src/ruleset.rs
+++ src/ruleset.rs
@@ -21,31 +21,37 @@ impl SRuleSet {
 }
 
 pub fn read_ruleset() -> [SRuleSet; 4] {
+    let path = Path::new(".schafkopfruleset");
+    if !path.exists() {
+        println!("File {} not found. Creating it.", path.display());
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("Could not create {}: {}", path.display(), Error::description(&why)),
+            Ok(file) => file,
+        };
+        // TODO: make creation of ruleset file adjustable
+        file.write_all(b"rufspiel\n").unwrap();
+        file.write_all(b"solo\n").unwrap();
+    }
     create_playerindexmap(|eplayerindex| {
         let mut vecrules = Vec::<Box<TRules>>::new();
-        let path = Path::new(".schafkopfruleset");
-        if path.exists() {
-            let file = match File::open(&path) {
-                Err(why) => panic!("Could not open {}: {}", path.display(), Error::description(&why)),
-                Ok(file) => file,
-            };
-            for str_l in BufReader::new(&file).lines().map(|str| str.unwrap()) {
-                println!("allowing rule: {}", str_l);
-                if str_l=="rufspiel" {
-                    for efarbe in EFarbe::all_values().iter().filter(|&efarbe| EFarbe::Herz!=*efarbe) {
-                        vecrules.push(Box::new(CRulesRufspiel{m_eplayerindex: eplayerindex, m_efarbe: *efarbe}));
-                    }
-                } else if str_l=="solo" {
-                    for efarbe in EFarbe::all_values().iter() {
-                        vecrules.push(Box::new(CRulesSolo{m_eplayerindex: eplayerindex, m_efarbe: *efarbe}));
-                    }
-                } else {
-                    println!("{} is not a valid rule descriptor", str_l);
+        assert!(path.exists()); 
+        let file = match File::open(&path) {
+            Err(why) => panic!("Could not open {}: {}", path.display(), Error::description(&why)),
+            Ok(file) => file,
+        };
+        for str_l in BufReader::new(&file).lines().map(|str| str.unwrap()) {
+            println!("allowing rule: {}", str_l);
+            if str_l=="rufspiel" {
+                for efarbe in EFarbe::all_values().iter().filter(|&efarbe| EFarbe::Herz!=*efarbe) {
+                    vecrules.push(Box::new(CRulesRufspiel{m_eplayerindex: eplayerindex, m_efarbe: *efarbe}));
                 }
+            } else if str_l=="solo" {
+                for efarbe in EFarbe::all_values().iter() {
+                    vecrules.push(Box::new(CRulesSolo{m_eplayerindex: eplayerindex, m_efarbe: *efarbe}));
+                }
+            } else {
+                println!("{} is not a valid rule descriptor", str_l);
             }
-        } else {
-            println!("File not found. Creating it.");
-            unimplemented!();
         }
         SRuleSet { m_vecrules : vecrules }
     })
