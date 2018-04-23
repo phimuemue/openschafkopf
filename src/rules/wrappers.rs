dc795fef54c1d742063515d25ba24f2d8b1b2838
openschafkopf <openschafkopf>
2018-04-23 18:41:42 +0200

    Wrappers for references implemented as tuple structs

--- src/rules/wrappers.rs
+++ src/rules/wrappers.rs
@@ -3,37 +3,29 @@ use primitives::*;
 // thin wrappers ensuring invariants
 
 #[derive(Copy, Clone)]
-pub struct SFullHand<'hand> {
-    hand: &'hand SHand,
-}
+pub struct SFullHand<'hand>(&'hand SHand);
 
 impl<'hand> SFullHand<'hand> {
     pub fn new(hand: &SHand, ekurzlang: EKurzLang) -> SFullHand {
         assert_eq!(hand.cards().len(), ekurzlang.cards_per_player());
-        SFullHand {
-            hand,
-        }
+        SFullHand(hand)
     }
     pub fn get(&self) -> &SHand {
-        self.hand
+        self.0
     }
 }
 
 #[derive(Copy, Clone)]
-pub struct SGameFinishedStiche<'vecstich> {
-    vecstich: &'vecstich [SStich],
-}
+pub struct SGameFinishedStiche<'vecstich>(&'vecstich [SStich]);
 
 impl<'vecstich> SGameFinishedStiche<'vecstich> {
     pub fn new(vecstich: &[SStich], ekurzlang: EKurzLang) -> SGameFinishedStiche {
         assert_eq!(vecstich.len(), ekurzlang.cards_per_player());
         assert!(vecstich.iter().all(|stich| 4==stich.size()));
-        SGameFinishedStiche {
-            vecstich,
-        }
+        SGameFinishedStiche(vecstich)
     }
     pub fn get(&self) -> &[SStich] {
-        self.vecstich
+        self.0
     }
 }
 
