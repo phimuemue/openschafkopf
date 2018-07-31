af30e8d38d5bfe9c14419a7ce6ef6efea4eb56ce
openschafkopf <openschafkopf>
2018-07-31 21:37:18 +0200

    Extend detect_expensive_all_possible_hands to inspect suspicions

--- src/ai/test.rs
+++ src/ai/test.rs
@@ -7,7 +7,10 @@ use player::{
 };
 use util::*;
 use handiterators::*;
-use ai::is_compatible_with_game_so_far;
+use ai::{
+    is_compatible_with_game_so_far,
+    suspicion::*,
+};
 
 #[test]
 fn detect_expensive_all_possible_hands() {
@@ -17,28 +20,43 @@ fn detect_expensive_all_possible_hands() {
                 let vecstich = game.completed_stichs();
                 if game.kurzlang().cards_per_player() - 4 < vecstich.len() {
                     let epi_fixed = verify!(game.current_stich().current_playerindex()).unwrap();
-                    let n_count_possible_hands = all_possible_hands(
+                    let vecahand = all_possible_hands(
                         vecstich,
                         game.ahand[epi_fixed].clone(),
                         epi_fixed,
                     )
                         .filter(|ahand| is_compatible_with_game_so_far(ahand, game.rules.as_ref(), &game.vecstich))
-                        .count();
-                    assert!(n_count_possible_hands < /*arbitrary bound, hoping that exploration for lower values can be done efficiently*/3000,
-                        "n_count_possible_hands: {}, rules: {}\nahand: {:#?}\nvecstich:{:?}",
-                        n_count_possible_hands,
-                        game.rules,
-                        game.ahand.iter()
-                            .map(ToString::to_string)
-                            .collect::<Vec<_>>(),
-                        game.vecstich.iter()
-                            .map(ToString::to_string)
-                            .collect::<Vec<_>>(),
-                    )
+                        .collect::<Vec<_>>();
+                    let assert_bound = |n, n_detect| {
+                        assert!(n < n_detect,
+                            "n: {}\nrules: {}\nahand: {:#?}\nvecstich:{:?}",
+                            n,
+                            game.rules,
+                            game.ahand.iter()
+                                .map(ToString::to_string)
+                                .collect::<Vec<_>>(),
+                            game.vecstich.iter()
+                                .map(ToString::to_string)
+                                .collect::<Vec<_>>(),
+                        );
+                    };
+                    assert_bound(vecahand.len(), 2000);
+                    for ahand in vecahand {
+                        assert_bound(
+                            SSuspicion::new(
+                                game.current_stich().first_playerindex(),
+                                ahand,
+                                game.rules.as_ref(),
+                                &mut game.completed_stichs().to_vec(),
+                                &|_vecstich_complete, _vecstich_successor| {/*no filtering*/},
+                            ).count_leaves(),
+                            2000,
+                        );
+                    }
                 }
             },
         )) as Box<TPlayer>),
-        /*n_games*/400,
+        /*n_games*/4,
         &verify!(SRuleSet::from_string(
             r"
             base-price=10
