d2718a0a626fcba0a694ae84ea5bbe8463d43fd5
philipp <descpl@yahoo.de>
2020-04-23 21:09:37 +0200

    External command's success checked in closure

--- main/build.rs
+++ main/build.rs
@@ -10,29 +10,30 @@ use std::{
 
 fn main() {
     // adapted from https://doc.rust-lang.org/cargo/reference/build-scripts.html#case-study-code-generation
-    let execute_external = |cmd: &mut Command| debug_verify!(cmd.output()).unwrap();
+    let execute_external = |cmd: &mut Command| {
+        let output = debug_verify!(cmd.output()).unwrap();
+        assert!(output.status.success(), output);
+        output
+    };
     let path_resources = Path::new(&debug_verify!(env::var("CARGO_MANIFEST_DIR")).unwrap()) // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
         .join("tools");
     let str_env_var_out_dir = debug_verify!(env::var("OUT_DIR")).unwrap();
     let path_out_dir = Path::new(&str_env_var_out_dir); // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
     // TODO can we avoid lessc depencency?
-    let output_lessc = execute_external(
-        Command::new("lessc")
-            .arg(path_resources.join("css.less"))
-    );
-    assert!(output_lessc.status.success(), output_lessc);
     debug_verify!(
         debug_verify!(
             File::create(&path_out_dir.join("css.css"))
         ).unwrap()
-            .write_all(&output_lessc.stdout)
+            .write_all(&execute_external(
+                Command::new("lessc")
+                    .arg(path_resources.join("css.less"))
+            ).stdout)
     ).unwrap();
     // TODO can we avoid inkscape depencency?
-    let output_inkscape = execute_external(
+    execute_external(
         Command::new("inkscape")
             .arg(path_resources.join("cards.svg"))
             .arg(format!("--export-png={}", debug_verify!(path_out_dir.join("cards.png").to_str()).unwrap()))
     );
-    assert!(output_inkscape.status.success(), output_inkscape);
 }
 
