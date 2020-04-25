46fd84bec7a312ddeb97c1012780c4f3f9a91bbc
philipp <descpl@yahoo.de>
2020-04-25 17:42:34 +0200

    Cargo fmt monitor

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -1,69 +1,80 @@
-use openschafkopf_util::*;
-use openschafkopf_logging::*;
 use as_num::AsNum;
 use byteorder::ByteOrder;
+use itertools::Itertools;
+use openschafkopf_logging::*;
+use openschafkopf_util::*;
 use serde_json::json;
 use std::io::{Read, Write};
-use itertools::Itertools;
 
 fn main() -> Result<(), failure::Error> {
     openschafkopf_logging::init_logging()?;
     use std::sync::{Arc, Mutex};
-    let ocmd_openschafkopf : Arc<Mutex<Option<std::process::Child>>> = Arc::new(Mutex::new(None));
+    let ocmd_openschafkopf: Arc<Mutex<Option<std::process::Child>>> = Arc::new(Mutex::new(None));
     let (sendstr, recvstr) = std::sync::mpsc::channel();
     std::thread::spawn(move || {
         while let Ok(str_openschafkopf_out) = recvstr.recv() {
-            let str_json_out = json!({
-                "strOpenschafkopfOut": str_openschafkopf_out
-            }).to_string();
+            let str_json_out = json!({ "strOpenschafkopfOut": str_openschafkopf_out }).to_string();
             info!("Trying to send \"{}\"", str_json_out);
-            debug_verify!(std::io::stdout().write(&via_out_param(|abyte_buffer_msg_len: &mut [u8; 4]|
-                byteorder::NativeEndian::write_u32(abyte_buffer_msg_len, str_json_out.len().as_num::<u32>())
-            ).0)).unwrap();
+            debug_verify!(std::io::stdout().write(
+                &via_out_param(|abyte_buffer_msg_len: &mut [u8; 4]| {
+                    byteorder::NativeEndian::write_u32(
+                        abyte_buffer_msg_len,
+                        str_json_out.len().as_num::<u32>(),
+                    )
+                })
+                .0
+            ))
+            .unwrap();
             debug_verify!(std::io::stdout().write(str_json_out.as_bytes())).unwrap();
             debug_verify!(std::io::stdout().flush()).unwrap();
         }
     });
     loop {
         let str_json_in = {
-            const N_BYTES_FOR_MSG_LEN : usize = 4;
-            let (abyte_buffer_msg_len, n_bytes_read) = via_out_param(|abyte_buffer_msg_len: &mut [u8; N_BYTES_FOR_MSG_LEN]|
-                debug_verify!(std::io::stdin().read(abyte_buffer_msg_len)).unwrap()
-            );
+            const N_BYTES_FOR_MSG_LEN: usize = 4;
+            let (abyte_buffer_msg_len, n_bytes_read) =
+                via_out_param(|abyte_buffer_msg_len: &mut [u8; N_BYTES_FOR_MSG_LEN]| {
+                    debug_verify!(std::io::stdin().read(abyte_buffer_msg_len)).unwrap()
+                });
             match n_bytes_read {
                 0 => {
                     info!("Received 0 bytes. Exiting.");
                     return Ok(());
-                },
+                }
                 N_BYTES_FOR_MSG_LEN => {
                     let n_bytes_msg_len = byteorder::NativeEndian::read_u32(&abyte_buffer_msg_len);
                     let str_json_in = debug_verify!(String::from_utf8(
                         debug_verify!(via_out_param_init_result(
                             (0..n_bytes_msg_len).map(|_| 0).collect::<Vec<_>>(),
                             |vecbyte| std::io::stdin().read(vecbyte)
-                        )).unwrap().0
-                    )).unwrap();
+                        ))
+                        .unwrap()
+                        .0
+                    ))
+                    .unwrap();
                     info!("Received \"{}\"", str_json_in);
                     str_json_in
-                },
-                _ => {
-                    panic!("Unexpected value for n_bytes_read: {}", n_bytes_read)
                 }
+                _ => panic!("Unexpected value for n_bytes_read: {}", n_bytes_read),
             }
         };
         let communicate_error = |str_error_msg| {
             warn!("Communicating error: {}", str_error_msg);
-            debug_verify!(sendstr.send(json!({
-                "Err": {
-                    "strErrorMsg": str_error_msg,
-                    "strInput": str_json_in
-                }
-            }).to_string()/*TODO? better to avoid digression via json value?*/)).unwrap();
+            debug_verify!(sendstr.send(
+                json!({
+                    "Err": {
+                        "strErrorMsg": str_error_msg,
+                        "strInput": str_json_in
+                    }
+                })
+                .to_string() /*TODO? better to avoid digression via json value?*/
+            ))
+            .unwrap();
         };
         match serde_json::de::from_str::<serde_json::Value>(&str_json_in) {
             Ok(jsonval) => {
-                if let Some(mut cmd_openschafkopf) = debug_verify!(ocmd_openschafkopf.lock()).unwrap()
-                    .take()
+                if let Some(mut cmd_openschafkopf) =
+                    debug_verify!(ocmd_openschafkopf.lock()).unwrap().take()
                 {
                     if let Ok(()) = cmd_openschafkopf.kill() {
                         communicate_error("Process did not finish early enough.");
@@ -87,13 +98,13 @@ fn main() -> Result<(), failure::Error> {
                 let str_selected_game_name = json_get!("selectedGameName", as_str);
                 let jsonarr_announcement = json_get!("announcements", as_array);
                 let n_epi_active = {
-                    match jsonarr_announcement.iter()
+                    match jsonarr_announcement
+                        .iter()
                         .enumerate()
                         .filter(|&(_n_epi, jsonval_announcement)| jsonval_announcement.is_string())
-                        .exactly_one() {
-                        Ok((n_epi_active, _str_announcement)) => {
-                            n_epi_active
-                        },
+                        .exactly_one()
+                    {
+                        Ok((n_epi_active, _str_announcement)) => n_epi_active,
                         Err(e) => {
                             communicate_error(&format!("No single announcement: {:?}", e));
                             continue;
@@ -150,14 +161,16 @@ fn main() -> Result<(), failure::Error> {
                 let stdout = debug_verify!(cmd_openschafkopf.stdout.take()).unwrap();
                 *debug_verify!(ocmd_openschafkopf.lock()).unwrap() = Some(cmd_openschafkopf);
                 std::thread::spawn(move || {
-                    if let Ok((str_openschafkopf_out, _n_bytes)) = via_out_param_result(|str_openschafkopf_out|
-                        std::io::BufReader::new(stdout).read_to_string(str_openschafkopf_out)
-                    ) {
+                    if let Ok((str_openschafkopf_out, _n_bytes)) =
+                        via_out_param_result(|str_openschafkopf_out| {
+                            std::io::BufReader::new(stdout).read_to_string(str_openschafkopf_out)
+                        })
+                    {
                         debug_verify!(sendstr.send(str_openschafkopf_out)).unwrap();
                         debug_verify!(ocmd_openschafkopf.lock()).unwrap().take();
                     }
                 });
-            },
+            }
             Err(e) => {
                 communicate_error(&format!("{:?} (category {:?})", e, e.classify()));
             }
