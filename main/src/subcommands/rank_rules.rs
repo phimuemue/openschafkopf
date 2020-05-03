8d329025f0bc03fc8f488aa4a3209e79d2a74c03
philipp <descpl@yahoo.de>
2020-05-03 12:57:08 +0200

    Improve output of rank-rules

--- main/src/subcommands/rank_rules.rs
+++ main/src/subcommands/rank_rules.rs
@@ -1,22 +1,27 @@
 use crate::ai::*;
 use crate::primitives::*;
 use crate::rules::ruleset::*;
+use crate::util::*;
 
 pub fn rank_rules(ruleset: &SRuleSet, hand: SFullHand, epi: EPlayerIndex, ai: &SAi) {
-    for rules in allowed_rules(&ruleset.avecrulegroup[epi], hand)
-        .filter_map(|orules| orules) // do not rank None
-    {
-        println!("Hand: {}", hand.get());
-        println!("{}: {}",
-            rules,
-            ai.rank_rules(
-                hand,
-                EPlayerIndex::EPI0,
-                epi,
-                rules.upcast(),
-                /*tpln_stoss_doubling*/(0, 0), // assume no stoss, no doublings in subcommand rank-rules
-                /*n_stock*/0, // assume no stock in subcommand rank-rules
+    println!("Hand: {}", hand.get());
+    let mut vecpairrulesf = allowed_rules(&ruleset.avecrulegroup[epi], hand)
+        .filter_map(|orules| orules.map(|rules| { // do not rank None
+            (
+                rules,
+                ai.rank_rules(
+                    hand,
+                    EPlayerIndex::EPI0,
+                    epi,
+                    rules.upcast(),
+                    /*tpln_stoss_doubling*/(0, 0), // assume no stoss, no doublings in subcommand rank-rules
+                    /*n_stock*/0, // assume no stock in subcommand rank-rules
+                ),
             )
-        );
+        }))
+        .collect::<Vec<_>>();
+    vecpairrulesf.sort_unstable_by(|pairrulesf_lhs, pairrulesf_rhs| debug_verify!(pairrulesf_rhs.1.partial_cmp(&pairrulesf_lhs.1)).unwrap());
+    for (rules, f_avg_payout) in vecpairrulesf {
+        println!("{}: {}", rules, f_avg_payout);
     }
 }
