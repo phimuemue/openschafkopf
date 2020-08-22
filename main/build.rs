740539c28af01d6f1e7c398ea99e8139bee317a5
philipp <descpl@yahoo.de>
2020-08-22 15:36:29 +0200

    Simplify build.rs

--- main/build.rs
+++ main/build.rs
@@ -9,24 +9,27 @@ fn main() {
         assert!(output.status.success(), output);
         output
     };
-    let path_resources = Path::new(&debug_verify!(env::var("CARGO_MANIFEST_DIR")).unwrap()) // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
-        .join("tools");
+    let path_resources = Path::new("tools");
     let str_env_var_out_dir = debug_verify!(env::var("OUT_DIR")).unwrap();
     let path_out_dir = Path::new(&str_env_var_out_dir); // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
     // TODO can we avoid lessc depencency?
+    let path_css_in = path_resources.join("css.less");
+    println!("cargo:rerun-if-changed={}", debug_verify!(path_css_in.to_str()).unwrap());
     debug_verify!(
         debug_verify!(
             File::create(&path_out_dir.join("css.css"))
         ).unwrap()
             .write_all(&execute_external(
                 Command::new("lessc")
-                    .arg(path_resources.join("css.less"))
+                    .arg(path_css_in)
             ).stdout)
     ).unwrap();
     // TODO can we avoid inkscape depencency?
+    let path_svg_in = path_resources.join("cards.svg");
+    println!("cargo:rerun-if-changed={}", debug_verify!(path_svg_in.to_str()).unwrap());
     execute_external(
         Command::new("inkscape")
-            .arg(path_resources.join("cards.svg"))
+            .arg(path_svg_in)
             .arg(format!("--export-png={}", debug_verify!(path_out_dir.join("cards.png").to_str()).unwrap()))
     );
 }
