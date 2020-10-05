e6dc65ce3484c96108e918510fb4817d65ac9c4f
philipp <descpl@yahoo.de>
2020-10-05 16:03:05 +0200

    analyze_plain supports multiple games in one file

--- main/src/subcommands/analyze.rs
+++ main/src/subcommands/analyze.rs
@@ -6,7 +6,7 @@ use crate::util::*;
 use std::io::Read;
 use itertools::Itertools;
 
-pub fn analyze_html(str_html: &str) -> Result<SAnalyzeParams, failure::Error> {
+pub fn analyze_sauspiel_html(str_html: &str) -> Result<SAnalyzeParams, failure::Error> {
     use combine::{char::*, *};
     use select::{document::Document, node::Node, predicate::*};
     let doc = Document::from(&str_html as &str);
@@ -150,46 +150,50 @@ pub fn analyze_html(str_html: &str) -> Result<SAnalyzeParams, failure::Error> {
     })
 }
 
-fn analyze_plain(str_plain: &str) -> Result<SAnalyzeParams, failure::Error> {
-    let (str_rules, str_cards) = str_plain
-        .split(":")
-        .collect_tuple()
-        .ok_or_else(|| format_err!("':' does not separate rules from stichs."))?;
-    let str_cards = str_cards.trim();
-    let rules = crate::rules::parser::parse_rule_description(
-        str_rules,
-        (/*n_tarif_extra*/10, /*n_tarif_ruf*/10, /*n_tarif_solo*/50), // TODO? make customizable
-        /*fn_player_to_epi*/|str_epi| EPlayerIndex::checked_from_usize(str_epi.parse()?)
-            .ok_or_else(|| format_err!("Cannot convert {} to EPlayerIndex.", str_epi)),
-    )?;
-    let veccard = parse_cards::<Vec<_>>(str_cards)
-        .ok_or_else(|| format_err!("Could not parse cards: {}", str_cards))?;
-    let stichseq = veccard.iter().fold(
-        SStichSequence::new(
-            /*ekurzlang*/EKurzLang::values()
-                .find(|ekurzlang| ekurzlang.cards_per_player()*EPlayerIndex::SIZE==veccard.len())
-                .ok_or_else(|| format_err!("Incorrect number of cards: {}", veccard.len()))?
-        ),
-        mutate_return!(|stichseq, &card| {
-            stichseq.zugeben(card, rules.as_ref());
+fn analyze_plain<'str_lines>(str_lines: &'str_lines str) -> impl Iterator<Item=Result<SAnalyzeParams, failure::Error>> + 'str_lines {
+    str_lines
+        .lines()
+        .map(|str_plain| {
+            let (str_rules, str_cards) = str_plain
+                .split(":")
+                .collect_tuple()
+                .ok_or_else(|| format_err!("':' does not separate rules from stichs."))?;
+            let str_cards = str_cards.trim();
+            let rules = crate::rules::parser::parse_rule_description(
+                str_rules,
+                (/*n_tarif_extra*/10, /*n_tarif_ruf*/10, /*n_tarif_solo*/50), // TODO? make customizable
+                /*fn_player_to_epi*/|str_epi| EPlayerIndex::checked_from_usize(str_epi.parse()?)
+                    .ok_or_else(|| format_err!("Cannot convert {} to EPlayerIndex.", str_epi)),
+            )?;
+            let veccard = parse_cards::<Vec<_>>(str_cards)
+                .ok_or_else(|| format_err!("Could not parse cards: {}", str_cards))?;
+            let stichseq = veccard.iter().fold(
+                SStichSequence::new(
+                    /*ekurzlang*/EKurzLang::values()
+                        .find(|ekurzlang| ekurzlang.cards_per_player()*EPlayerIndex::SIZE==veccard.len())
+                        .ok_or_else(|| format_err!("Incorrect number of cards: {}", veccard.len()))?
+                ),
+                mutate_return!(|stichseq, &card| {
+                    stichseq.zugeben(card, rules.as_ref());
+                })
+            );
+            Ok(SAnalyzeParams {
+                rules,
+                ahand: EPlayerIndex::map_from_fn(|epi|
+                    SHand::new_from_vec(
+                        stichseq
+                            .completed_stichs()
+                            .iter()
+                            .map(|stich| stich[epi])
+                            .collect()
+                    )
+                ),
+                vecn_doubling: vec![],
+                vecn_stoss: vec![],
+                n_stock: 0,
+                vecstich: stichseq.completed_stichs().to_vec(),
+            })
         })
-    );
-    Ok(SAnalyzeParams {
-        rules,
-        ahand: EPlayerIndex::map_from_fn(|epi|
-            SHand::new_from_vec(
-                stichseq
-                    .completed_stichs()
-                    .iter()
-                    .map(|stich| stich[epi])
-                    .collect()
-            )
-        ),
-        vecn_doubling: vec![],
-        vecn_stoss: vec![],
-        n_stock: 0,
-        vecstich: stichseq.completed_stichs().to_vec(),
-    })
 }
 
 pub fn analyze<
@@ -201,17 +205,33 @@ pub fn analyze<
             match globresult {
                 Ok(path) => {
                     println!("Opening {:?}", path);
-                    vecanalyzeparams.push(SAnalyzeParamsWithDesc{
-                        str_description: path.to_string_lossy().into_owned(),
-                        str_link: format!("file://{}", path.to_string_lossy()),
-                        resanalyzeparams: {
-                            let str_input = &via_out_param_result(|str_html|
-                                std::fs::File::open(&path)?.read_to_string(str_html)
-                            )?.0;
-                            analyze_html(&str_input)
-                                .or_else(|_e| analyze_plain(&str_input))
-                        },
-                    });
+                    let str_input = &via_out_param_result(|str_html|
+                        std::fs::File::open(&path)?.read_to_string(str_html)
+                    )?.0;
+                    let mut b_found = false;
+                    let mut push_analyzeparams = |resanalyzeparams: Result<_, _>| {
+                        b_found = b_found || resanalyzeparams.is_ok();
+                        vecanalyzeparams.push(SAnalyzeParamsWithDesc{
+                            str_description: path.to_string_lossy().into_owned(),
+                            str_link: format!("file://{}", path.to_string_lossy()),
+                            resanalyzeparams,
+                        });
+                    };
+                    if let resanalyzeparams@Ok(_) = analyze_sauspiel_html(&str_input) {
+                        push_analyzeparams(resanalyzeparams)
+                    } else {
+                        let mut b_found_plain = false;
+                        for resanalyzeparams in analyze_plain(&str_input).filter(|res| res.is_ok()) {
+                            b_found_plain = true;
+                            push_analyzeparams(resanalyzeparams)
+                        }
+                        if !b_found_plain {
+                            push_analyzeparams(Err(format_err!("Nothing found in {:?}: Trying to continue.", path)));
+                        }
+                    }
+                    if !b_found {
+                        println!("Nothing found in {:?}: Trying to continue.", path);
+                    }
                 },
                 Err(e) => {
                     println!("Error: {:?}. Trying to continue.", e);
