8b44af43063b9a2ff27c3d45784b8357ec4f8e70
philipp <descpl@yahoo.de>
2022-01-13 16:03:02 +0100

    Convert games to to neural network input (3) update script

--- scripts/convert_to_rules_stichseq.sh
+++ scripts/convert_to_rules_stichseq.sh
@@ -5,7 +5,7 @@
 for STR_FILE in `ls ../openschafkopf_training_data/raw`
 do
     7z x ../openschafkopf_training_data/raw/$STR_FILE -otmp
-    ./target/release/openschafkopf parse tmp/**/*.html > ../openschafkopf_training_data/rules_stichseq/$STR_FILE.txt
+    ./target/release/openschafkopf parse tmp/**/*.html
     rm -rf tmp
 done
 
