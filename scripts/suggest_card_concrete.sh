14207038f98535c3514a49a24a91abf4fcdebfcc
philipp <descpl@yahoo.de>
2022-03-24 22:05:34 +0100

    Add test script for concrete hand iterator
    This might be useful when we want to test whether certain heuristics
    offer a real benefit.

---
+++ scripts/suggest_card_concrete.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# to be run from the openschafkopf directory
+
+cargo build --release -j16
+
+hyperfine -r1 --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8   go hk h8 h7   SA sk' --hand 'so HU h9 ga gk e9' --rules 'eichel rufspiel von 2' --branching 'equiv7' --simulate-hands 'GU su G9 HA SZ S9   EK G8 HO S7 HZ   EO EU GZ G7 S8'"
+hyperfine -r1  --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8'  --hand 'go so HU h9 ga gk e9' --rules 'eichel rufspiel von 2' --branching equiv7 --simulate-hands 'GU su G9 HA SZ S9 h7   hk EK G8 HO S7 HZ sk  h8 EO EU GZ G7 S8 sa'"
+
