2d7caf9161670a4853da4436a281040edf3d6dd4
philipp <descpl@yahoo.de>
2022-04-10 12:25:58 +0200

    Analysis groups cards (3) remove resolved TODO

--- main/src/game_analysis/mod.rs
+++ main/src/game_analysis/mod.rs
@@ -295,7 +295,6 @@ pub fn generate_analysis_html(
                 &analysispercard.determinebestcardresult_cheating,
                 /*fn_human_readable_payout*/&|f_payout| f_payout,
             );
-            // TODO group vecoutputline to show equivalent cards in same row
             // TODO? show unplayable cards as separate row
             // TODO simplify output (as it currently only shows results from one ahand)
             let mut str_per_card = format!(r###"<h3>{}</h3>"###, stich_caption(&analysispercard.stichseq));
