db386417a68240611e1a695cc1d89920d7e8f046
philipp <descpl@yahoo.de>
2022-04-14 05:46:14 +0200

    More timings in script suggest_card_concrete

--- scripts/suggest_card_concrete.sh
+++ scripts/suggest_card_concrete.sh
@@ -5,5 +5,6 @@
 cargo build --release -j16
 
 hyperfine -r1 --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8   go hk h8 h7   SA sk' --hand 'so HU h9 ga gk e9' --rules 'eichel rufspiel von 2' --branching 'equiv7' --simulate-hands 'GU su G9 HA SZ S9   EK G8 HO S7 HZ   EO EU GZ G7 S8'"
+hyperfine -r1  --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8  go hk h8 h7'  --hand 'EO EU GZ G7 S8 sa' --rules 'eichel rufspiel von 2' --branching equiv7 --simulate-hands 'so HU h9 ga gk e9  GU su G9 HA SZ S9  EK G8 HO S7 HZ sk'"
 hyperfine -r1  --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8'  --hand 'go so HU h9 ga gk e9' --rules 'eichel rufspiel von 2' --branching equiv7 --simulate-hands 'GU su G9 HA SZ S9 h7   hk EK G8 HO S7 HZ sk  h8 EO EU GZ G7 S8 sa'"
 
