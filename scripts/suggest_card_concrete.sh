450fac5f69f8d44f1b726875a79f2d187d83962e
philipp <descpl@yahoo.de>
2022-05-01 22:27:54 +0200

    suggest_card scripts compile only suggest-card

--- scripts/suggest_card_concrete.sh
+++ scripts/suggest_card_concrete.sh
@@ -2,7 +2,7 @@
 
 # to be run from the openschafkopf directory
 
-cargo build --release -j16
+cargo build --release -j16 --no-default-features --features suggest-card
 
 hyperfine -r1 --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8   go hk h8 h7   SA sk' --hand 'so HU h9 ga gk e9' --rules 'eichel rufspiel von 2' --branching 'equiv7' --simulate-hands 'GU su G9 HA SZ S9   EK G8 HO S7 HZ   EO EU GZ G7 S8'"
 hyperfine -r1  --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8  go hk h8 h7'  --hand 'EO EU GZ G7 S8 sa' --rules 'eichel rufspiel von 2' --branching equiv7 --simulate-hands 'so HU h9 ga gk e9  GU su G9 HA SZ S9  EK G8 HO S7 HZ sk'"
--- scripts/suggest_card_expensive_ramsch.sh
+++ scripts/suggest_card_expensive_ramsch.sh
@@ -2,6 +2,6 @@
 
 # to be run from the openschafkopf directory
 
-cargo build --release -j16
+cargo build --release -j16 --no-default-features --features suggest-card
 
 hyperfine -r1 --show-output 'target/release/openschafkopf suggest-card --hand "h8 su eu ga eo gk" --cards-on-table "g9 g7 gz g8  e8 e9 ez ek" --simulate-hands "gu ha go s9 ea e7  ho h9 sz s7 sk s8  so hz hk sa h7 hu" --rules "ramsch" --branching equiv7'
