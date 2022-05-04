cf5be6e1eb51a7e2591252fd86967f87dd43b92f
philipp <descpl@yahoo.de>
2022-05-04 13:01:56 +0200

    Improve user input (4.4) User can specify all hands at once (4) scripts use new specification method

--- scripts/suggest_card_concrete.sh
+++ scripts/suggest_card_concrete.sh
@@ -4,7 +4,7 @@
 
 cargo build --release -j16 --no-default-features --features suggest-card
 
-hyperfine -r1 --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8   go hk h8 h7   SA sk' --hand 'so HU h9 ga gk e9' --rules 'eichel rufspiel von 2' --branching 'equiv7' --simulate-hands 'GU su G9 HA SZ S9   EK G8 HO S7 HZ   EO EU GZ G7 S8'"
-hyperfine -r1  --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8  go hk h8 h7'  --hand 'EO EU GZ G7 S8 sa' --rules 'eichel rufspiel von 2' --branching equiv7 --simulate-hands 'so HU h9 ga gk e9  GU su G9 HA SZ S9  EK G8 HO S7 HZ sk'"
-hyperfine -r1  --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8'  --hand 'go so HU h9 ga gk e9' --rules 'eichel rufspiel von 2' --branching equiv7 --simulate-hands 'GU su G9 HA SZ S9 h7   hk EK G8 HO S7 HZ sk  h8 EO EU GZ G7 S8 sa'"
+hyperfine -r1 --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8   go hk h8 h7   SA sk' --hand 'GU su G9 HA SZ S9   EK G8 HO S7 HZ   EO EU GZ G7 S8  so HU h9 ga gk e9' --rules 'eichel rufspiel von 2' --branching 'equiv7'"
+hyperfine -r1  --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8  go hk h8 h7'  --hand 'so HU h9 ga gk e9  EO EU GZ G7 S8 sa  GU su G9 HA SZ S9  EK G8 HO S7 HZ sk' --rules 'eichel rufspiel von 2' --branching equiv7"
+hyperfine -r1  --show-output -- "./target/release/openschafkopf suggest-card --cards-on-table 'e7 ea ez e8'  --hand 'GU su G9 HA SZ S9 h7  go so HU h9 ga gk e9  hk EK G8 HO S7 HZ sk  h8 EO EU GZ G7 S8 sa' --rules 'eichel rufspiel von 2' --branching equiv7"
 
--- scripts/suggest_card_expensive_ramsch.sh
+++ scripts/suggest_card_expensive_ramsch.sh
@@ -4,4 +4,4 @@
 
 cargo build --release -j16 --no-default-features --features suggest-card
 
-hyperfine -r1 --show-output 'target/release/openschafkopf suggest-card --hand "h8 su eu ga eo gk" --cards-on-table "g9 g7 gz g8  e8 e9 ez ek" --simulate-hands "gu ha go s9 ea e7  ho h9 sz s7 sk s8  so hz hk sa h7 hu" --rules "ramsch" --branching equiv7'
+hyperfine -r1 --show-output 'target/release/openschafkopf suggest-card --hand "gu ha go s9 ea e7  h8 su eu ga eo gk  ho h9 sz s7 sk s8  so hz hk sa h7 hu" --cards-on-table "g9 g7 gz g8  e8 e9 ez ek" --rules "ramsch" --branching equiv7'
