7ae0d8d61ceb25ae3c6eaaa2354409909938809d
philipp <descpl@yahoo.de>
2022-07-13 09:42:38 +0200

    stich oracle (10) Add bench_stichoracle.sh

---
+++ scripts/bench_stichoracle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cargo build -j16 --release --no-default-features --features suggest-card
+hyperfine -r1 -- \
+    "./target/release/openschafkopf suggest-card --rules 'rufspiel eichel von 0' --cards-on-table 'go eo ho so  sz sa s9' --hand 'gu ha hk h7 ez gz gk  EU E7 H9 EA E8 SK  HU G7 E9 H8 HZ G8  SU EK GA S8 S7 G9' --simulate-hands all --branching equiv6" \
+    "./target/release/openschafkopf suggest-card --rules 'rufspiel eichel von 0' --cards-on-table 'go eo ho so  sz sa s9' --hand 'gu ha hk h7 ez gz gk  EU E7 H9 EA E8 SK  HU G7 E9 H8 HZ G8  SU EK GA S8 S7 G9' --simulate-hands all --branching oracle"
+
+hyperfine -r1 -- \
+    "./target/release/openschafkopf suggest-card --rules 'rufspiel eichel von 0' --cards-on-table 'go eo ho so' --hand 'gu ha hk h7 ez gz gk  SZ EU E7 H9 EA E8 SK  SA HU G7 E9 H8 HZ G8  S9 SU EK GA S8 S7 G9' --simulate-hands all --branching equiv6" \
+    "./target/release/openschafkopf suggest-card --rules 'rufspiel eichel von 0' --cards-on-table 'go eo ho so' --hand 'gu ha hk h7 ez gz gk  SZ EU E7 H9 EA E8 SK  SA HU G7 E9 H8 HZ G8  S9 SU EK GA S8 S7 G9' --simulate-hands all --branching oracle"
