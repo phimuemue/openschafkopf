ea0bed86af23bf3e265f0eac7186715e8e1fbece
philipp <descpl@yahoo.de>
2022-08-06 16:23:53 +0200

    Add script to test snapshotcache

---
+++ scripts/bench_snapshotcache.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cargo build -j16 --release --no-default-features --features suggest-card
+
+hyperfine -r1 --show-output -- \
+    "./target/release/openschafkopf suggest-card --rules 'Rufspiel mit der Gras-Sau von 3' --cards-on-table 'so ha eu h8' --hand 'H9 SK SZ G7 GA EA SU  H7 HZ HK HU SA GK EK  GO G8 S8 S9 S7 E9 EZ  EO E8 GZ G9 E7 GU HO' --branching equiv6 --simulate-hands all --snapshotcache" \
+    "./target/release/openschafkopf suggest-card --rules 'Rufspiel mit der Gras-Sau von 3' --cards-on-table 'so ha eu h8' --hand 'H9 SK SZ G7 GA EA SU  H7 HZ HK HU SA GK EK  GO G8 S8 S9 S7 E9 EZ  EO E8 GZ G9 E7 GU HO' --branching equiv6 --simulate-hands all"
+
