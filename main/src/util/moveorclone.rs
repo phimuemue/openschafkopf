6e772dbd19905e154620961e2d4061ed3f5330c4
philipp <descpl@yahoo.de>
2022-11-19 14:37:29 +0100

    Introduce TMoveOrClone

---
+++ main/src/util/moveorclone.rs
@@ -0,0 +1,19 @@
+// I expected to find something like this in Rust's stdlib (possibly in std::borrow), but the documentation did not show anything.
+// It may be because, usually, functions are better off by just accepting the target type.
+// However, there *may* be valid use cases for this trait, so keep it for now, and see if it proves useful.
+
+pub trait TMoveOrClone<Target> {
+    fn move_or_clone(self) -> Target;
+}
+
+impl<T> TMoveOrClone<T> for T {
+    fn move_or_clone(self) -> T {
+        self
+    }
+}
+
+impl<'t, T: Clone> TMoveOrClone<T> for &'t T {
+    fn move_or_clone(self) -> T {
+        self.clone()
+    }
+}
