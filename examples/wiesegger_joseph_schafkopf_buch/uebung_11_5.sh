ede707417c6ebaf1e30cf89c59b9095caa0b1e57
philipp <descpl@yahoo.de>
2022-12-28 23:47:48 +0100

    Introduce who_has_<card> scripting function (3)

--- examples/wiesegger_joseph_schafkopf_buch/uebung_11_5.sh
+++ examples/wiesegger_joseph_schafkopf_buch/uebung_11_5.sh
@@ -1,3 +1,3 @@
 ./target/release/openschafkopf suggest-card --rules "Herz-Wenz von 0" --cards-on-table "gu ho h7 h9  h8 eu sz sk  ga g7 go ha  hu e7 g8 s9  eo gz e9" --hand "ea ek e8 so" --simulate-hands all --branching equiv7 --verbose
 echo "We confirm that Eichel-Zehn is at player 2"
-./target/release/openschafkopf hand-stats --rules "Herz-Wenz von 0" --cards-on-table "gu ho h7 h9  h8 eu sz sk  ga g7 go ha  hu e7 g8 s9  eo gz e9" --hand "ea ek e8 so" --simulate-hands all --inspect "/*TODO introduce who_has_ez*/ctx.ez()"
+./target/release/openschafkopf hand-stats --rules "Herz-Wenz von 0" --cards-on-table "gu ho h7 h9  h8 eu sz sk  ga g7 go ha  hu e7 g8 s9  eo gz e9" --hand "ea ek e8 so" --simulate-hands all --inspect "ctx.who_has_ez()"
