b262009d567b06873b18d8afcfe313beb04e33c8
philipp <descpl@yahoo.de>
2023-01-01 14:03:34 +0100

    Add benchmark script
    Currently ~5 seconds on my machine.

---
+++ scripts/bench_suggest_card_wenz.sh
@@ -0,0 +1,3 @@
+set -e
+
+hyperfine './target/release/openschafkopf suggest-card --hand "hu sz sk s9 ha h9  ea e9 ga gz g9 _  _ _ so ez eo _  eu sa ek gk go hk" --rules "wenz von 0" --snapshotcache --branching equiv7'
