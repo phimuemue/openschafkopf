79cc647364ce5dc384fe0a75b57087585925a9c3
philipp <descpl@yahoo.de>
2023-03-28 11:49:42 +0200

    Improve wiesegger_joseph_schafkopf_buch/uebung_10_3.sh

--- examples/wiesegger_joseph_schafkopf_buch/uebung_10_3.sh
+++ examples/wiesegger_joseph_schafkopf_buch/uebung_10_3.sh
@@ -1,3 +1,23 @@
 # Remark: The solution's argument for Gras-Koenig is somewhat convincing - but the software cannot capture it as of now.
 
-./target/release/openschafkopf suggest-card --rules "Eichel-Wenz von 2" --cards-on-table "ha hk ea h8  gu ez eu e7  sk s7 ek sa  su g7 e8 hu" --hand "ga gk go g8" --simulate-hands all --branching "9,9" --verbose
+./target/release/openschafkopf suggest-card --rules "Eichel-Wenz von 2" --cards-on-table "ha hk ea h8  gu ez eu e7  sk s7 ek sa  su g7 e8 hu" --hand "ga gk go g8" --simulate-hands all --branching "9,9" --verbose --points --snapshotcache
+
+echo ""
+echo "The above shows that we cannot win this game by our own efforts."
+echo ""
+echo "Links' cards can be determined:"
+./target/release/openschafkopf hand-stats --rules "Eichel-Wenz von 2" --cards-on-table "ha hk ea h8  gu ez eu e7  sk s7 ek sa  su g7 e8 hu" --hand "ga gk go g8" --simulate-hands all --inspect "ctx.hand_to_string(2)"
+
+echo ""
+echo "Run simulation from Links' point of view, depending on the card we play:"
+for card in GA GK GO G8
+do
+    echo "If we play $card:"
+    ./target/release/openschafkopf suggest-card \
+        --rules "Eichel-Wenz von 2" \
+        --cards-on-table "ha hk ea h8  gu ez eu e7  sk s7 ek sa  su g7 e8 hu $card" \
+        --hand "eo e9 gz g9" \
+        --no-details \
+        --points --snapshotcache
+done
+echo "The above shows that we should not play Gras-Ass but some other card."
