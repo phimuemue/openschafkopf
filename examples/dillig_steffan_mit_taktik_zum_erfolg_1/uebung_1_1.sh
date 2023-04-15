968c6451474142add5d42b885d5c54188247b650
philipp <descpl@yahoo.de>
2023-04-15 19:52:06 +0200

    Accept multiple --rules (2) use it in examples

--- examples/dillig_steffan_mit_taktik_zum_erfolg_1/uebung_1_1.sh
+++ examples/dillig_steffan_mit_taktik_zum_erfolg_1/uebung_1_1.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./target/release/openschafkopf suggest-card --rules "0 spielt mit der Schell" --hand "eo so su hz h7  gz gk  s7" --simulate-hands 20 --branching oracle --verbose --points --snapshotcache --no-details
-./target/release/openschafkopf suggest-card --rules "0 spielt mit der Gras" --hand "eo so su hz h7  gz gk  s7" --simulate-hands 20 --branching oracle --verbose --points --snapshotcache --no-details
+./target/release/openschafkopf suggest-card --rules "0 spielt mit der Schell" --rules "0 spielt mit der Gras" --hand "eo so su hz h7  gz gk  s7" --simulate-hands 20 --branching oracle --points --snapshotcache --no-details
--- examples/dillig_steffan_mit_taktik_zum_erfolg_2/uebung_4_3.sh
+++ examples/dillig_steffan_mit_taktik_zum_erfolg_2/uebung_4_3.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-./target/release/openschafkopf suggest-card --rules "3 spielt Eichel-Wenz" --hand "eu gu  ha hz h7  ea ek e7" --simulate-hands 20 --branching oracle --points --snapshotcache --verbose --position 3 --no-details
-./target/release/openschafkopf suggest-card --rules "3 spielt Herz-Wenz" --hand "eu gu  ha hz h7  ea ek e7" --simulate-hands 20 --branching oracle --points --snapshotcache --position 3 --no-details
-./target/release/openschafkopf suggest-card --rules "3 spielt Wenz" --hand "eu gu  ha hz h7  ea ek e7" --simulate-hands 20 --branching oracle --points --snapshotcache --position 3 --no-details
+./target/release/openschafkopf suggest-card --rules "3 spielt Eichel-Wenz" --rules "3 spielt Herz-Wenz" --rules "3 spielt Wenz" --hand "eu gu  ha hz h7  ea ek e7" --simulate-hands 20 --branching oracle --points --snapshotcache --position 3 --no-details
 
