e6c5f80838a2b7834a093b6408b58c2e8f55581f
philipp <descpl@yahoo.de>
2023-06-19 12:19:49 +0200

    TRuleSpecificAI only needs hand, stichseq (1) SAIRufspiel uses stichseq

--- main/src/ai/rulespecific/airufspiel.rs
+++ main/src/ai/rulespecific/airufspiel.rs
@@ -11,7 +11,7 @@ pub struct SAIRufspiel<'rules, RufspielPayout: TRufspielPayout> {
 
 impl<RufspielPayout: TRufspielPayout> TRuleSpecificAI for SAIRufspiel<'_, RufspielPayout> {
     fn suggest_card(&self, game: &SGame) -> Option<ECard> {
-        let epi = unwrap!(game.which_player_can_do_something()).0;
+        let epi = unwrap!(game.stichseq.current_playable_stich().current_playerindex());
         let rules = self.rules;
         // suchen
         if epi!=rules.active_playerindex() && game.stichseq.no_card_played() {
@@ -20,7 +20,7 @@ impl<RufspielPayout: TRufspielPayout> TRuleSpecificAI for SAIRufspiel<'_, Rufspi
                 let veccard_ruffarbe : Vec<_> = hand.cards().iter().copied()
                     .filter(|&card| rules.trumpforfarbe(card)==rules.trumpforfarbe(rules.rufsau()))
                     .collect();
-                match (veccard_ruffarbe.len(), game.kurzlang()) {
+                match (veccard_ruffarbe.len(), game.stichseq.kurzlang()) {
                     (0, _kurzlang) => return None,
                     (1, _kurzlang) => return Some(veccard_ruffarbe[0]),
                     (2, EKurzLang::Kurz) => return verify!(veccard_ruffarbe.into_iter().max_by_key(|&card| points_card(card))),
