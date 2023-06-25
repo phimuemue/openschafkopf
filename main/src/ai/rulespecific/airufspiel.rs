dccb8f82fad9b31d0141aef6a80baa22165f6afe
philipp <descpl@yahoo.de>
2023-06-25 07:29:50 +0200

    Lint: Collapsible if statement

--- main/src/ai/rulespecific/airufspiel.rs
+++ main/src/ai/rulespecific/airufspiel.rs
@@ -12,19 +12,21 @@ impl<RufspielPayout: TRufspielPayout> TRuleSpecificAI for SAIRufspiel<'_, Rufspi
     fn suggest_card(&self, hand: &SHand, stichseq: &SStichSequence) -> Option<ECard> {
         let rules = self.rules;
         // suchen
-        if unwrap!(stichseq.current_playable_stich().current_playerindex())!=rules.active_playerindex() && stichseq.no_card_played() {
-            if !hand.contains(rules.rufsau()) {
-                let veccard_ruffarbe : Vec<_> = hand.cards().iter().copied()
-                    .filter(|&card| rules.trumpforfarbe(card)==rules.trumpforfarbe(rules.rufsau()))
-                    .collect();
-                match (veccard_ruffarbe.len(), stichseq.kurzlang()) {
-                    (0, _kurzlang) => return None,
-                    (1, _kurzlang) => return Some(veccard_ruffarbe[0]),
-                    (2, EKurzLang::Kurz) => return verify!(veccard_ruffarbe.into_iter().max_by_key(|&card| points_card(card))),
-                    (2, EKurzLang::Lang) => return verify!(veccard_ruffarbe.into_iter().min_by_key(|&card| points_card(card))),
-                    (3 | 4, EKurzLang::Lang) => return verify!(veccard_ruffarbe.into_iter().max_by_key(|&card| points_card(card))),
-                    _ => panic!("Found too many ruffarbe cards"),
-                }
+        if 
+            unwrap!(stichseq.current_playable_stich().current_playerindex())!=rules.active_playerindex()
+            && stichseq.no_card_played()
+            && !hand.contains(rules.rufsau())
+        {
+            let veccard_ruffarbe : Vec<_> = hand.cards().iter().copied()
+                .filter(|&card| rules.trumpforfarbe(card)==rules.trumpforfarbe(rules.rufsau()))
+                .collect();
+            match (veccard_ruffarbe.len(), stichseq.kurzlang()) {
+                (0, _kurzlang) => return None,
+                (1, _kurzlang) => return Some(veccard_ruffarbe[0]),
+                (2, EKurzLang::Kurz) => return verify!(veccard_ruffarbe.into_iter().max_by_key(|&card| points_card(card))),
+                (2, EKurzLang::Lang) => return verify!(veccard_ruffarbe.into_iter().min_by_key(|&card| points_card(card))),
+                (3 | 4, EKurzLang::Lang) => return verify!(veccard_ruffarbe.into_iter().max_by_key(|&card| points_card(card))),
+                _ => panic!("Found too many ruffarbe cards"),
             }
         }
         None
