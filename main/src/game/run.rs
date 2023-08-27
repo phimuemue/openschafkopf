0ffd8b87970a8d45beebb42773d764c23eb2a6d5
philipp <descpl@yahoo.de>
2023-08-27 07:19:40 +0200

    Move test_game_loop to game/run.rs

--- main/src/game/run.rs
+++ main/src/game/run.rs
@@ -148,4 +148,115 @@ pub fn run_simple_game_loop(
     )
 }
 
+#[test]
+fn test_game_loop() {
+    use rand::prelude::IteratorRandom;
+    use crate::ai;
+    use crate::player::{
+        *,
+        playercomputer::*,
+    };
+    let mut rng = rand::thread_rng();
+    use itertools::iproduct;
+    for ruleset in
+        iproduct!(
+            [10, 20], // n_base_price
+            [50, 100], // n_solo_price
+            [2, 3], // n_lauf_min
+            [ // str_allowed_games
+                r"
+                [rufspiel]
+                [solo]
+                [wenz]
+                lauf-min=2
+                ",
+                r"
+                [solo]
+                [farbwenz]
+                [wenz]
+                [geier]
+                ",
+                r"
+                [solo]
+                [wenz]
+                [bettel]
+                ",
+                r"
+                [solo]
+                [wenz]
+                [bettel]
+                stichzwang=true
+                ",
+            ],
+            [ // str_no_active_game
+                r"[ramsch]
+                price=20
+                ",
+                r"[ramsch]
+                price=50
+                durchmarsch = 75",
+                r#"[ramsch]
+                price=50
+                durchmarsch = "all""#,
+                r"[stock]",
+                r"[stock]
+                price=30",
+                r"",
+                r#"[ramsch]
+                price=20
+                jungfrau="DoubleAll"
+                "#,
+                r#"[ramsch]
+                price=20
+                jungfrau="DoubleIndividuallyOnce"
+                "#,
+                r#"[ramsch]
+                price=20
+                jungfrau="DoubleIndividuallyMultiple"
+                "#,
+            ],
+            [ // str_extras
+                r"[steigern]",
+                r"[steigern]
+                step=15
+                ",
+                r"[doubling]",
+                r#"deck = "kurz""#,
+                r"[stoss]",
+                r"[stoss]
+                max=3
+                ",
+            ]
+        )
+            .map(|(n_base_price, n_solo_price, n_lauf_min, str_allowed_games, str_no_active_game, str_extras)| {
+                let str_ruleset = format!(
+                    "base-price={}
+                    solo-price={}
+                    lauf-min={}
+                    {}
+                    {}
+                    {}",
+                    n_base_price, n_solo_price, n_lauf_min, str_allowed_games, str_no_active_game, str_extras
+                );
+                println!("{}", str_ruleset);
+                unwrap!(crate::rules::ruleset::SRuleSet::from_string(&str_ruleset))
+            })
+            .choose_multiple(&mut rng, 2)
+    {
+        run_simple_game_loop(
+            EPlayerIndex::map_from_fn(|epi| -> Box<dyn TPlayer> {
+                Box::new(SPlayerComputer{ai: {
+                    if epi<EPlayerIndex::EPI2 {
+                        ai::SAi::new_cheating(/*n_rank_rules_samples*/1, /*n_suggest_card_branches*/2)
+                    } else {
+                        ai::SAi::new_simulating(/*n_rank_rules_samples*/1, /*n_suggest_card_branches*/1, /*n_suggest_card_samples*/1)
+                    }
+                }})
+            }),
+            /*n_games*/4,
+            ruleset,
+            /*fn_print_account_balance*/|_,_| {/* no output */},
+        );
+    }
+}
 
