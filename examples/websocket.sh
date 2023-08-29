599ce36b3c3b0c9dc4d43e5ff9ef00d82500c726
philipp <descpl@yahoo.de>
2023-08-29 22:27:34 +0200

    openschafkopf_lib (15) examples/websocket.sh
    TODO? Maybe move all the "site" things into an independent folder.

--- examples/websocket.sh
+++ examples/websocket.sh
@@ -2,9 +2,9 @@
 sleep 2 # time to start openschafkopf
 
 # try to open for four players, each requiring to enter a name
-sensible-browser main/tools/site/site.html
-sensible-browser main/tools/site/site.html
-sensible-browser main/tools/site/site.html
-sensible-browser main/tools/site/site.html
+sensible-browser lib/tools/site/site.html
+sensible-browser lib/tools/site/site.html
+sensible-browser lib/tools/site/site.html
+sensible-browser lib/tools/site/site.html
 
 echo "Browser should have opened for four players. Beware that openschafkopf is running as a background process."
