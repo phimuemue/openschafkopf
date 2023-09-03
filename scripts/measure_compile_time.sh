7d49611233d4980bf2aa33a0f9462d8e0a706814
philipp <descpl@yahoo.de>
2023-09-03 17:10:30 +0200

    Update measure_compile_time.sh

--- scripts/measure_compile_time.sh
+++ scripts/measure_compile_time.sh
@@ -2,6 +2,7 @@
 
 # to be run from the openschafkopf directory
 
+find lib -name *.rs -exec touch '{}' \;
 find main -name *.rs -exec touch '{}' \;
-hyperfine -r1 -- "cargo build --release -j16"
+hyperfine -r1 -- "cargo build -j16"
 
