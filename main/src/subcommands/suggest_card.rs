d52a275faf953bbc5e0ba76089a961bad745519d
philipp <descpl@yahoo.de>
2023-11-17 16:56:23 +0100

    Omittable strategies (13) use generics in suggest_card

--- main/src/subcommands/suggest_card.rs
+++ main/src/subcommands/suggest_card.rs
@@ -272,8 +272,18 @@ pub fn run(clapmatches: &clap::ArgMatches) -> Result<(), Error> {
                         }),
                     },
                     match (clapmatches.value_of("prune")) {
-                        Some("hint") => (SMinReachablePayoutLowerBoundViaHint),
-                        _ => (SMinReachablePayout),
+                        Some("hint") => (
+                            SMinReachablePayoutBase::<
+                                SPrunerViaHint,
+                                SPerMinMaxStrategyHigherKinded,
+                            >
+                        ),
+                        _ => (
+                            SMinReachablePayoutBase::<
+                                SPrunerNothing,
+                                SPerMinMaxStrategyHigherKinded,
+                            >
+                        ),
                     },
                     match (clapmatches.is_present("snapshotcache")) { // TODO customizable depth
                         true => (
@@ -294,7 +304,7 @@ pub fn run(clapmatches: &clap::ArgMatches) -> Result<(), Error> {
                 )
             }}
             if clapmatches.is_present("no-details") {
-                macro_rules! forward{((($($func_filter_allowed_cards_ty: tt)*), $func_filter_allowed_cards: expr), ($foreachsnapshot: ident), ($fn_snapshotcache:expr), $fn_visualizer: expr,) => {{ // TODORUST generic closures
+                macro_rules! forward{((($($func_filter_allowed_cards_ty: tt)*), $func_filter_allowed_cards: expr), ($foreachsnapshot: ty), ($fn_snapshotcache:expr), $fn_visualizer: expr,) => {{ // TODORUST generic closures
                     itahand
                         .enumerate()
                         .par_bridge() // TODO can we derive a true parallel iterator?
@@ -304,7 +314,7 @@ pub fn run(clapmatches: &clap::ArgMatches) -> Result<(), Error> {
                                 (&mut ahand.clone(), &mut stichseq.clone()),
                                 rules,
                                 &$func_filter_allowed_cards,
-                                &$foreachsnapshot::new(
+                                &<$foreachsnapshot>::new(
                                     rules,
                                     epi_position,
                                     expensifiers.clone(),
@@ -348,7 +358,7 @@ pub fn run(clapmatches: &clap::ArgMatches) -> Result<(), Error> {
                 }
             } else {
                 let determinebestcardresult = { // we are interested in payout => single-card-optimization useless
-                    macro_rules! forward{((($($func_filter_allowed_cards_ty: tt)*), $func_filter_allowed_cards: expr), ($foreachsnapshot: ident), ($fn_snapshotcache:expr), $fn_visualizer: expr,) => {{ // TODORUST generic closures
+                    macro_rules! forward{((($($func_filter_allowed_cards_ty: tt)*), $func_filter_allowed_cards: expr), ($foreachsnapshot: ty), ($fn_snapshotcache:expr), $fn_visualizer: expr,) => {{ // TODORUST generic closures
                         let n_repeat_hand = clapmatches.value_of("repeat_hands").unwrap_or("1").parse()?;
                         determine_best_card::<$($func_filter_allowed_cards_ty)*,_,_,_,_,_,_,_>( // TODO avoid explicit types
                             stichseq,
@@ -363,7 +373,7 @@ pub fn run(clapmatches: &clap::ArgMatches) -> Result<(), Error> {
                                     })
                             ) as Box<_>,
                             $func_filter_allowed_cards,
-                            &$foreachsnapshot::new(
+                            &<$foreachsnapshot>::new(
                                 rules,
                                 epi_position,
                                 expensifiers.clone(),
