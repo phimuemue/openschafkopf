9799cf78894063d597b1809d7e3829a72eee9c69
philipp <descpl@yahoo.de>
2023-11-18 23:46:24 +0100

    dyn TRules -> SRules (5) eliminate Box-related utilities

--- lib/src/util/mod.rs
+++ lib/src/util/mod.rs
@@ -1,13 +1,11 @@
 pub use as_num::AsNum;
 pub use plain_enum::*;
 #[macro_use]
-pub mod box_clone;
-#[macro_use]
 pub mod staticvalue;
 pub mod interval;
 pub mod vecext;
 pub mod negext;
-pub use self::{box_clone::*, staticvalue::*, interval::*, if_then::*, vecext::*, negext::*, dbg_argument::*};
+pub use self::{staticvalue::*, interval::*, if_then::*, vecext::*, negext::*, dbg_argument::*};
 pub use derive_new::new;
 pub use failure::{bail, format_err, Error};
 pub use openschafkopf_util::*;
@@ -15,30 +13,6 @@ pub use openschafkopf_util::*;
 pub mod bitfield;
 #[macro_use]
 pub mod dbg_argument;
-
-// TODORUST Objects should be upcastable to supertraits: https://github.com/rust-lang/rust/issues/5665
-#[allow(unused_macros)]
-macro_rules! make_upcastable {
-    ($upcasttrait:ident, $trait:ident) => {
-        pub trait $upcasttrait {
-            fn upcast<'retval, 'slf: 'retval>(&'slf self) -> &'retval dyn $trait;
-            fn upcast_box(self: Box<Self>) -> Box<dyn $trait>
-            where
-                Self: 'static;
-        }
-        impl<T: $trait> $upcasttrait for T {
-            fn upcast<'retval, 'slf: 'retval>(&'slf self) -> &'retval dyn $trait {
-                self
-            }
-            fn upcast_box(self: Box<Self>) -> Box<dyn $trait>
-            where
-                Self: 'static,
-            {
-                self as Box<dyn $trait>
-            }
-        }
-    };
-}
 
 pub fn tpl_flip_if<T>(b: bool, (t0, t1): (T, T)) -> (T, T) {
     if b {
