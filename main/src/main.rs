592c8ce2f55386cbe54979b5ab771243aba6fd9a
philipp <descpl@yahoo.de>
2024-02-14 22:09:54 +0100

    blocks_in_if_conditions -> blocks_in_conditions

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(feature = "cargo-clippy", allow(
-    clippy::blocks_in_if_conditions,
+    clippy::blocks_in_conditions,
     clippy::clone_on_copy, // TODORUST I think that some types that implement Copy should not implement it (in particular: large arrays)
 ))]
 
