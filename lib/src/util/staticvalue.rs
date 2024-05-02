1d38648010c5f18ed5a9f060e1122eb4d043541a
philipp <descpl@yahoo.de>
2024-05-02 18:22:51 +0200

    Eliminate unused TStaticValue

--- lib/src/util/staticvalue.rs
+++ lib/src/util/staticvalue.rs
@@ -1,17 +1,7 @@
-use std::fmt;
-
-// TODORUST this should become superfluous once we have const generics
-pub trait TStaticValue<V> : Sync + 'static + Clone + fmt::Debug + Send {
-    const VALUE : V;
-}
-
 macro_rules! define_static_value {
     (pub $struct: ident, $type: ty, $value: expr) => {
         #[derive(Copy, Clone, Debug, Default)]
         pub struct $struct {}
-        impl TStaticValue<$type> for $struct {
-            const VALUE: $type = $value;
-        }
         impl TStaticOrDynamicValue<$type> for $struct {
             fn value(self) -> $type {
                 $value
