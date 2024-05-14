b303c804a2f2fde89ad59ab5e2b43e33d1156968
philipp <descpl@yahoo.de>
2024-05-14 19:27:07 +0200

    hand-stats prints weighted sum

--- main/src/subcommands/hand_stats.rs
+++ main/src/subcommands/hand_stats.rs
@@ -22,39 +22,99 @@ pub fn run(clapmatches: &clap::ArgMatches) -> Result<(), Error> {
                 .map_err(|_| format_err!("Cannot parse inspection target."))
         })
         .collect::<Result<Vec<_>,_>>()?;
+    #[derive(Clone, Copy)]
+    struct STotalOrderedFloat(rhai::FLOAT); // TODO good idea?
+    impl Eq for STotalOrderedFloat {}
+    impl PartialEq for STotalOrderedFloat {
+        fn eq(&self, other: &STotalOrderedFloat) -> bool {
+            self.0.total_cmp(&other.0)==std::cmp::Ordering::Equal
+        }
+    }
+    impl std::hash::Hash for STotalOrderedFloat {
+        fn hash<H>(&self, state: &mut H)
+            where H: std::hash::Hasher
+        {
+            self.0.to_bits().hash(state)
+        }
+    }
+    #[derive(Hash, Eq, PartialEq)]
+    enum VInspectionResult<Number> {
+        Number(Number),
+        Unknown(String),
+    }
+    #[derive(Hash, Eq, PartialEq)]
+    enum VIntFloat<Float> {
+        Int(rhai::INT),
+        Float(Float),
+    }
     with_common_args(
         clapmatches,
         |itahand, rules, _stichseq, _ahand_fixed_with_holes, _epi_position, _expensifiers, b_verbose| {
-            let mut vectplmapresstrnconstraint = vecconstraint
+            let mut vectplmapresinspectionresultnconstraint = vecconstraint
                 .iter()
                 .map(|constraint| (std::collections::HashMap::new(), constraint))
                 .collect::<Vec<_>>();
             let mut n_ahand_total = 0;
             for ahand in itahand {
                 // assert_eq!(ahand[epi_position], hand_fixed);
-                for (mapresstrn, constraint) in vectplmapresstrnconstraint.iter_mut() {
-                    *mapresstrn.entry(
+                for (mapresinspectionresultn, constraint) in vectplmapresinspectionresultnconstraint.iter_mut() {
+                    *mapresinspectionresultn.entry(
                         constraint.internal_eval(
                             &ahand,
                             rules.clone(),
-                            |resdynamic| resdynamic
-                                .map(|dynamic| dynamic.to_string())
+                            |resinspectionresult| resinspectionresult
+                                .map(|dynamic| {
+                                    if let Ok(n) = dynamic.as_int() {
+                                        VInspectionResult::Number(VIntFloat::Int(n))
+                                    } else if let Ok(f) = dynamic.as_float() {
+                                        VInspectionResult::Number(VIntFloat::Float(STotalOrderedFloat(f)))
+                                    } else {
+                                        VInspectionResult::Unknown(dynamic.to_string())
+                                    }
+                                })
                                 .map_err(|err| format!("Error: {:?}", err)),
                         ),
                     ).or_insert(0) += 1;
                 }
                 n_ahand_total += 1;
             }
-            for (mapresstrn, constraint) in vectplmapresstrnconstraint {
+            for (mapresinspectionresultn, constraint) in vectplmapresinspectionresultnconstraint {
                 if b_verbose || 1<vecconstraint.len() {
                     println!("{}", constraint);
                 }
-                for (resstr, n_count) in mapresstrn.into_iter()
+                let mut of_weighted_sum = Some(0.);
+                for (resstr, n_count, f_percentage) in mapresinspectionresultn.into_iter()
+                    .map(|(resinspectionresult, n_count)| {
+                        let f_percentage = n_count.as_num::<f64>()/n_ahand_total.as_num::<f64>();
+                        (
+                            resinspectionresult.map(|inspectionresult| match inspectionresult {
+                                VInspectionResult::Number(number) => {
+                                    let (str_result, f_summand) = match number {
+                                        VIntFloat::Int(n) => (format!("{}", n), n.as_num::<f64>()),
+                                        VIntFloat::Float(STotalOrderedFloat(f)) => (format!("{}", f), f.into()),
+                                    };
+                                    if let Some(ref mut f_weighted_sum) = of_weighted_sum {
+                                        *f_weighted_sum += f_summand * f_percentage;
+                                    };
+                                    str_result
+                                },
+                                VInspectionResult::Unknown(str_unknown) => {
+                                    of_weighted_sum = None;
+                                    str_unknown
+                                },
+                            }),
+                            n_count,
+                            f_percentage,
+                        )
+                    })
                     .sorted_unstable_by(|lhs, rhs| lhs.0.cmp(&rhs.0))
                 {
-                    println!("{} {} ({:.2}%)", resstr.unwrap_or_else(|err| err), n_count, (n_count.as_num::<f64>()/n_ahand_total.as_num::<f64>())*100.);
+                    println!("{} {} ({:.2}%)", resstr.unwrap_or_else(|err| err), n_count, f_percentage*100.);
                 }
-                    
+                if let Some(f_weighted_sum)=of_weighted_sum {
+                    println!("-----");
+                    println!("\u{2300} {:.2}", f_weighted_sum);
+                }
             }
             Ok(())
         }
