b08399f5a9dfbbdb203eff17690ae30ee6e22692
philipp <descpl@yahoo.de>
2025-05-19 22:00:09 +0200

    cargo update (4.2) fix getrandom in sauspiel_webext (2)
    TODO: Unify all the build/deploy scripts.

--- sauspiel_webext/webext/run_webext_build.sh
+++ sauspiel_webext/webext/run_webext_build.sh
@@ -1,4 +1,4 @@
 cd ..
-wasm-pack build --out-dir webext --target no-modules --no-typescript
+RUSTFLAGS='--cfg getrandom_backend="wasm_js"' wasm-pack build --out-dir webext --target no-modules --no-typescript
 cd webext
 web-ext build
